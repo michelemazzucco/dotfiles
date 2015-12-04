9b93268390c19ae9a6c55a2f39a0375a207e1897
Michele Mazzucco <hello@michelemazzucco.it>
2015-12-04 13:23:21 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+msg() {
+  printf "${yel} ${pur}$*${end}\n"
+}
+
+# Variables
+dir=~/.dotfiles
+now=`date +%Y-%m-%d-%H:%M:%S`
+olddir=$HOME/.dotfiles_old/$now
+files="gitconfig gitignore zshrc"
+
+### Create symlink and create files in homedir from .dotfiles folder 
+##msg "Create files..."
+##for file in $files; do
+##  ln -s $dir/$file $HOME/.$file
+##done
+
+# Create symlink and create files in .dotfiles folder from homedir 
+msg "Create files in .dotfiles..."
+for file in $files
+do
+  msg "Creating $file..."
+  ln -s ~/.$file $dir/$file
+done
