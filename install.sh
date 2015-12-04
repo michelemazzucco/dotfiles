becb94963ed509216bdcc266631b46693dac46e5
Michele Mazzucco <michele@Michele-MacBook-Pro.local>
2015-12-04 13:39:35 +0100

    test symlink

--- install.sh
+++ install.sh
@@ -6,18 +6,10 @@ msg() {
 dir=~/.dotfiles
 now=`date +%Y-%m-%d-%H:%M:%S`
 olddir=$HOME/.dotfiles_old/$now
-files="gitconfig gitignore zshrc"
+files="gitconfig gitignore"
 
-### Create symlink and create files in homedir from .dotfiles folder 
-##msg "Create files..."
-##for file in $files; do
-##  ln -s $dir/$file $HOME/$file
-##done
-
-# Create symlink and create files in .dotfiles folder from homedir 
-msg "Create files in .dotfiles..."
-for file in $files
-do
-  msg "Creating $file..."
-  ln -s ~/$file $dir/$file
+# Create symlink and create files in homedir from .dotfiles folder 
+msg "Create files..."
+for file in $files; do
+  ln -s $dir/$file $HOME/.$file
 done
