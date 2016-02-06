6a86a2614bb5309ead5c9012e4734657420ef9c2
Michele Mazzucco <hello@michelemazzucco.it>
2016-02-06 15:45:27 +0100

    remove utils folder and add a file

---
+++ utils.sh
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------------
+# Colors
+# -----------------------------------------------------------------
+export RED="\e[0;31m"
+export GREEN="\e[0;32m"
+export YELLOW="\e[1;33m"
+export BLUE="\e[0;34m"
+
+# -----------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------
+# Messages
+message() {
+  printf "$BLUE$*\n"
+}
+
+error() {
+  printf "$RED$*\n"
+}
+
+ok() {
+  printf "$GREEN...done!\n\n"
+}
+
+# All
+brew_clean() {
+  brew update && brew upgrade && brew cleanup && brew cask cleanup
+}
