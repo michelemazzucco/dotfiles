d3b2d33c587ec064482518ad0d729e096cde9b62
Michele Mazzucco <hello@michelemazzucco.it>
2016-03-21 11:38:56 +0100

    new version of ruby

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ fi
 if type "rbenv" > /dev/null; then
     message "Installing Ruby with rbenv..."
     # change ruby version if you want
-    rbenv install 2.0.0-p645 && rbenv local 2.0.0-p645 && rbenv rehash
+    rbenv install 2.2.4 && rbenv local 2.2.4 && rbenv rehash
     # install gems
     gem install sass && gem install jekyll && gem install bundle && gem install rails -v 4.2.4
     ok
