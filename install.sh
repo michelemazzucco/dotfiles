bf54498afe6ae6d1852202ef96cd4fdc1afc34e3
Michele Mazzucco <hello@michelemazzucco.it>
2016-09-23 10:15:22 +0200

    after sierra

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ setup_osx_conf() {
     read -r -p 'Are you sure to install my OSX configuration? [y/N] ' resp
     case $resp in
       [yY])
-        "./scripts/osx.sh"
+        "./scripts/macOs.sh"
         ;;
       *)
         message 'Ok, no problem!\n'
